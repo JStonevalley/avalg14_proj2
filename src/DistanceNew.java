56ff59c90ac173be5589b744e7eb10425d6e0267
Johan Arnör <johan.arnor@gmail.com>
2014-11-27 18:56:45 +0100

    A bit more efficient distance calculation.

--- src/DistanceNew.java
+++ src/DistanceNew.java
@@ -4,11 +4,13 @@ public class DistanceNew {
 	public int[][] calculateDistances(double[][] nodes) {
 		int[][] distances = new int[nodes.length][nodes.length];
 		for (int i = 0; i < nodes.length; i++) {
-			for (int j = 0; j < nodes.length; j++) {
+			for (int j = i; j < nodes.length; j++) { // j = i because of symmetric matrix
 				double cate1 = Math.abs(nodes[i][0] - nodes[j][0]);
 				double cate2 = Math.abs(nodes[i][1] - nodes[j][1]);
 				float hypotenuse = (float) Math.sqrt(cate1 * cate1 + cate2 * cate2);
-				distances[i][j] = Math.round(hypotenuse);
+				int distance = (int) (hypotenuse + 0.5f); // faster then Math.round()
+				distances[i][j] = distance;
+				distances[j][i] = distance;
 			}
 		}
 		return distances;
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.FileInputStream;
 
 public class Main {
 
-	private static final boolean DEBUG = true;
+	private static final boolean DEBUG = false;
 
 	public static void main(String[] args) throws Exception {
 		long startTime = System.nanoTime();
